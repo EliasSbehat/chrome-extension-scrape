c90a05e0ece9c676292348c96dc56f851541b4ad
elias <kompanipejper@gmail.com>
2023-06-09 23:54:13 +0300

    add feature

--- src/content.js
+++ src/content.js
@@ -6,29 +6,48 @@ chrome.runtime.onMessage.addListener(
             console.log("here");
             var list = $(".vbI").children();
             var result = [];
-            for (var j=0;j<list.length;j++) {
+            for (var j = 0; j < list.length; j++) {
                 var item = list[j];
                 var sub_top = $(item).children().children().children().children();
                 var fir_data = $(sub_top).children()[0];
-                $(fir_data).find("img").addClass("attachclass"+j);
-                var element = document.querySelector('.attachclass'+j);
+                $(fir_data).find("img").addClass("attachclass" + j);
+                var element = document.querySelector('.attachclass' + j);
                 // CREATING AN EVENT FOR HOVER
                 var event = new MouseEvent('mouseover', {
-                'view': window,
-                'bubbles': true,
-                'cancelable': true
+                    'view': window,
+                    'bubbles': true,
+                    'cancelable': true
                 });
 
                 // DISPATCHING THE EVENT, i.e., ACTUALLY HOVERING
                 element.dispatchEvent(event);
             }
-            setTimeout(function(){
+            setTimeout(async function () {
                 for (var i = 0; i < list.length; i++) {
+                    var largest_img = "";
+                    var highest_image = "";
                     var item = list[i];
                     var sub_top = $(item).children().children().children().children();
                     var fir_data = $(sub_top).children()[0];
                     var external_link = "";
-                    var image_link = $(fir_data).find("img").attr("src");
+                    var img = $(fir_data).find("img");
+                    var image_link = img.attr("src");
+                    largest_img = image_link;
+                    var largest = largest_img.replace("https://i.pinimg.com/", "");
+                    var parsePattern = largest.split("/")[0];
+                    highest_image = largest_img.replace(parsePattern, "originals");
+                    var confirmExist = await confirmExistImage(highest_image);
+                    if (confirmExist.msg=="no-exist") {
+                        highest_image = highest_image.replace("originals", "736x");
+                        var confirmExist2 = await confirmExistImage(highest_image);
+                        if (confirmExist2.msg=="no-exist") {
+                            highest_image = highest_image.replace("736x", "564x");
+                            var confirmExist3 = await confirmExistImage(highest_image);
+                            if (confirmExist3.msg=="no-exist") {
+                                highest_image = image_link;
+                            }
+                        }
+                    }
                     var name;
                     var vendor = "";
                     if (i === 0) {
@@ -53,21 +72,36 @@ chrome.runtime.onMessage.addListener(
                         name: name,
                         external_url: external_link,
                         image_link: image_link,
-                        vendor: vendor
+                        vendor: vendor,
+                        highest_image: highest_image
                     };
                     result.push(myObject);
                 }
                 const jsonResult = JSON.stringify(result);
                 console.log(result);
                 $.post(webhhook_url, { data: result }, function (res) {
-                        console.log(res);
-                    }
+                    console.log(res);
+                }
                 );
                 setTimeout(() => {
-                    sendResponse({ data: result });
+                    sendResponse({ data: "success" });
                 }, 1000);
             }, 1500);
             return true;
         }
     }
 );
+function confirmExistImage(image_link) {
+    return new Promise((resolve, reject) => {
+        const imgObj = new Image();
+        imgObj.src = image_link;
+        imgObj.onload = function () {
+            console.log('Image exists', image_link);
+            resolve({ msg: "exist" });
+        };
+        imgObj.onerror = function () {
+            // console.log('Image no exists', image_link);
+            resolve({ msg: "no-exist" });
+        };
+    });
+}
--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,20 @@
 (function () {
     'use strict';
     const submitBtn = document.getElementById('submit');
+
     submitBtn?.addEventListener('click', function handler(e) {
+        document.getElementById('submit').disabled = true;
+        document.getElementById('submit').innerText = "Proccessing...";
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             const activeTab = tabs[0];
             const message = { action: "scrap" };
-            chrome.tabs.sendMessage(activeTab.id, message);
+            chrome.tabs.sendMessage(activeTab.id, message, function (response) {
+                console.log('Received response:', response);
+                if (response.data) {
+                    document.getElementById('submit').disabled = false;
+                    document.getElementById('submit').innerText = "Scrape";
+                }
+            });
         });
     })
 })();
